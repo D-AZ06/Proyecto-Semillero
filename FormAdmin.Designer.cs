2f8c63edfb1cba1059ce9de708da595db0e4e806
KMH120 <kmedinah8@soy.sena.edu.co>
2026-03-19 14:51:21 -0500

    19/03/26, pantalla admin y clase consulta(vacía)

---
+++ FormAdmin.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Proyecto_Semillero
+{
+    partial class FormAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_gestionar_usuarios = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_gestionar_reportes = new Guna.UI2.WinForms.Guna2Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.dataGridView1 = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            this.lbl_gestionar = new System.Windows.Forms.Label();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Panel1
+            // 
+            this.Panel1.BorderRadius = 20;
+            this.Panel1.Controls.Add(this.label2);
+            this.Panel1.Controls.Add(this.btn_gestionar_reportes);
+            this.Panel1.Controls.Add(this.guna2Button2);
+            this.Panel1.Controls.Add(this.btn_gestionar_usuarios);
+            this.Panel1.Controls.Add(this.label1);
+            this.Panel1.Location = new System.Drawing.Point(13, 75);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(285, 568);
+            this.Panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bienvenido Administrador";
+            // 
+            // btn_gestionar_usuarios
+            // 
+            this.btn_gestionar_usuarios.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_gestionar_usuarios.BorderRadius = 15;
+            this.btn_gestionar_usuarios.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_gestionar_usuarios.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_gestionar_usuarios.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_gestionar_usuarios.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_gestionar_usuarios.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_gestionar_usuarios.ForeColor = System.Drawing.Color.White;
+            this.btn_gestionar_usuarios.Location = new System.Drawing.Point(44, 113);
+            this.btn_gestionar_usuarios.Name = "btn_gestionar_usuarios";
+            this.btn_gestionar_usuarios.Size = new System.Drawing.Size(188, 74);
+            this.btn_gestionar_usuarios.TabIndex = 2;
+            this.btn_gestionar_usuarios.Text = "👤 Usuarios ";
+            this.btn_gestionar_usuarios.Click += new System.EventHandler(this.btn_gestionar_usuarios_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BorderRadius = 15;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.Location = new System.Drawing.Point(42, 418);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(188, 74);
+            this.guna2Button2.TabIndex = 3;
+            this.guna2Button2.Text = "🌱 Semillero";
+            // 
+            // btn_gestionar_reportes
+            // 
+            this.btn_gestionar_reportes.BorderRadius = 15;
+            this.btn_gestionar_reportes.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_gestionar_reportes.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_gestionar_reportes.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_gestionar_reportes.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_gestionar_reportes.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btn_gestionar_reportes.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_gestionar_reportes.ForeColor = System.Drawing.Color.White;
+            this.btn_gestionar_reportes.Location = new System.Drawing.Point(44, 259);
+            this.btn_gestionar_reportes.Name = "btn_gestionar_reportes";
+            this.btn_gestionar_reportes.Size = new System.Drawing.Size(186, 74);
+            this.btn_gestionar_reportes.TabIndex = 4;
+            this.btn_gestionar_reportes.Text = "📄 Reportes";
+            this.btn_gestionar_reportes.Click += new System.EventHandler(this.btn_gestionar_reportes_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "¿Qué desea gestionar hoy?";
+            // 
+            // Panel2
+            // 
+            this.Panel2.Location = new System.Drawing.Point(-2, -8);
+            this.Panel2.Name = "Panel2";
+            this.Panel2.Size = new System.Drawing.Size(1111, 73);
+            this.Panel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Silver;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dataGridView1.Location = new System.Drawing.Point(32, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(717, 274);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridView1.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dataGridView1.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dataGridView1.ThemeStyle.BackColor = System.Drawing.Color.Silver;
+            this.dataGridView1.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dataGridView1.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.dataGridView1.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView1.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dataGridView1.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ThemeStyle.HeaderStyle.Height = 4;
+            this.dataGridView1.ThemeStyle.ReadOnly = false;
+            this.dataGridView1.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridView1.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dataGridView1.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView1.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.dataGridView1.ThemeStyle.RowsStyle.Height = 22;
+            this.dataGridView1.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dataGridView1.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // Panel3
+            // 
+            this.Panel3.Controls.Add(this.lbl_gestionar);
+            this.Panel3.Controls.Add(this.dataGridView1);
+            this.Panel3.Location = new System.Drawing.Point(318, 75);
+            this.Panel3.Name = "Panel3";
+            this.Panel3.Size = new System.Drawing.Size(775, 333);
+            this.Panel3.TabIndex = 3;
+            // 
+            // lbl_gestionar
+            // 
+            this.lbl_gestionar.AutoSize = true;
+            this.lbl_gestionar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_gestionar.Location = new System.Drawing.Point(358, 8);
+            this.lbl_gestionar.Name = "lbl_gestionar";
+            this.lbl_gestionar.Size = new System.Drawing.Size(61, 19);
+            this.lbl_gestionar.TabIndex = 7;
+            this.lbl_gestionar.Text = "Gestion";
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Location = new System.Drawing.Point(350, 445);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(703, 198);
+            this.guna2Panel1.TabIndex = 4;
+            // 
+            // FormAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Proyecto_Semillero.Properties.Resources._592593667718;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1105, 687);
+            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.Panel3);
+            this.Controls.Add(this.Panel2);
+            this.Controls.Add(this.Panel1);
+            this.Name = "FormAdmin";
+            this.Text = "FormAdmin";
+            this.Load += new System.EventHandler(this.FormAdmin_Load);
+            this.Panel1.ResumeLayout(false);
+            this.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Panel3.ResumeLayout(false);
+            this.Panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel Panel1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Button btn_gestionar_reportes;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button btn_gestionar_usuarios;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2Panel Panel2;
+        private Guna.UI2.WinForms.Guna2DataGridView dataGridView1;
+        private Guna.UI2.WinForms.Guna2Panel Panel3;
+        private System.Windows.Forms.Label lbl_gestionar;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+    }
+}
---
+++ FormAdmin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Semillero
+{
+    public partial class FormAdmin : Form
+    {
+        public FormAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAdmin_Load(object sender, EventArgs e)
+        {
+            Panel1.BackColor = Color.FromArgb(20, 255, 255, 255);
+            Panel2.BackColor = Color.FromArgb(20, 255, 255, 255);
+            Panel3.BackColor = Color.FromArgb(20, 255, 255, 255);
+        }
+
+        private void btn_gestionar_reportes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_gestionar_usuarios_Click(object sender, EventArgs e)
+        {
+            SqlCommand consulta2;
+            consulta2 = new SqlCommand("select * from Usuario", cn.conectar());//establecemos la consulta SQL para obtener todos los datos de la tabla Usuario
+            consulta2.CommandType = CommandType.Text;//establecemos el tipo de comando como texto
+            consulta2.ExecuteNonQuery();//ejecutamos la consulta SQL
+            ds.Clear();//limpiamos el DataSet para evitar que se acumulen los datos obtenidos de la consulta SQL
+            SqlDataAdapter da = new SqlDataAdapter(consulta2);//creamos un objeto de tipo SqlDataAdapter para ejecutar la consulta SQL y almacenar los datos obtenidos en el DataSet
+            da.Fill(ds, "Usuario");//llenamos el DataSet con los datos obtenidos de la consulta SQL
+            try
+            {
+                dataGridView1.DataMember = ("Usuario");//establecemos el miembro de datos del DataGridView para mostrar los datos de la tabla "Usuario" del DataSet
+                dataGridView1.DataSource = ds;//establecemos la fuente de datos del DataGridView como el DataSet para mostrar los datos obtenidos de la consulta SQL
+            }
+
+            catch (Exception ex)//capturamos cualquier excepción que pueda ocurrir al mostrar los datos en el DataGridView y mostramos un mensaje de error
+            {
+                MessageBox.Show(ex.Message);//mostramos el mensaje de error de la excepción
+            }
+        }
+    }
+}
